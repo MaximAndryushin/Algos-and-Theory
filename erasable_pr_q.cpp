57394fb3a9091d232b8e916e4d1ae72cd55b6c1e
Maxim Andryushin <unlimimage@gmail.com>
2024-03-06 02:41:29 +0300

    erasable priority queue

---
+++ erasable_pr_q.cpp
@@ -0,0 +1,32 @@
+#include <queue>
+
+
+template<typename T, typename _Compare = std::less<T>>
+struct EPQ {
+    void push(T val) {
+        ins.push(val);
+    }
+
+    const T& top() {
+        while(!rem.empty() && ins.top() == rem.top()) {
+            ins.pop();
+            rem.pop();
+        }
+        return ins.top();
+    }
+
+    void pop() {
+        ins.pop();
+    }
+
+    std::size_t size() {
+        return ins.size() - rem.size();
+    }
+
+    bool empty() {
+        return size() == 0;
+    }
+    
+private:
+    std::priority_queue<T, vector<T>, _Compare> ins, rem;
+};
